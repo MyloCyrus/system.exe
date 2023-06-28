19c4421e7e75d99fab39ac6e66a9ca1f87cc50f0
Mylo Cyrus <irahiggins740@gmail.com>
2023-06-28 14:59:30 -0700

    Add files via upload

---
+++ [system.exe].ps1
@@ -0,0 +1,300 @@
+ [system.exe]
+  TCP    192.168.249.49:57301   192.168.31.167:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57302   192.168.31.168:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57303   192.168.31.169:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57304   192.168.31.170:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57305   192.168.31.171:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57306   192.168.31.172:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57307   192.168.31.173:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57308   192.168.31.174:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57309   192.168.31.175:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57310   192.168.31.176:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57311   192.168.31.177:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57312   192.168.31.178:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57313   192.168.31.179:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57314   192.168.31.180:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57315   192.168.31.181:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57316   192.168.31.182:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57317   192.168.31.183:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57318   192.168.31.184:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57319   192.168.31.185:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57320   192.168.31.186:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57321   192.168.31.187:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57322   192.168.31.188:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57323   192.168.31.189:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57324   192.168.31.190:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57325   192.168.31.191:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57326   192.168.31.192:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57327   192.168.31.193:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57328   192.168.31.194:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57329   192.168.31.195:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57330   192.168.31.196:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57331   192.168.31.197:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57332   192.168.31.198:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57333   192.168.31.199:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57334   192.168.31.200:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57335   192.168.31.201:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57336   192.168.31.202:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57337   192.168.31.203:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57338   192.168.31.204:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57339   192.168.31.205:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57340   192.168.31.206:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57341   192.168.31.207:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57342   192.168.31.208:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57343   192.168.31.209:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57344   192.168.31.210:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57345   192.168.31.211:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57346   192.168.31.212:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57347   192.168.31.213:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57348   192.168.31.214:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57349   192.168.31.215:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57350   192.168.31.216:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57351   192.168.31.217:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57352   192.168.31.218:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57353   192.168.31.219:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57354   192.168.31.220:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57355   192.168.31.221:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57356   192.168.31.222:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57357   192.168.31.223:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57358   192.168.31.224:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57359   192.168.31.225:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57360   192.168.31.226:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57361   192.168.31.227:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57362   192.168.31.228:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57363   192.168.31.229:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57364   192.168.31.230:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    192.168.249.49:57365   192.168.31.231:microsoft-ds  SYN_SENT
+ [system.exe]
+  TCP    [::]:7                 mylocyrus:0            LISTENING
+ [tcpsvcs.exe]
+  TCP    [::]:9                 mylocyrus:0            LISTENING
+ [tcpsvcs.exe]
+  TCP    [::]:13                mylocyrus:0            LISTENING
+ [tcpsvcs.exe]
+  TCP    [::]:17                mylocyrus:0            LISTENING
+ [tcpsvcs.exe]
+  TCP    [::]:19                mylocyrus:0            LISTENING
+ [tcpsvcs.exe]
+  TCP    [::]:80                mylocyrus:0            LISTENING
+ Can not obtain ownership information
+  TCP    [::]:135               mylocyrus:0            LISTENING
+  RpcSs
+ [svchost.exe]
+  TCP    [::]:445               mylocyrus:0            LISTENING
+ Can not obtain ownership information
+  TCP    [::]:3587              mylocyrus:0            LISTENING
+  p2pimsvc
+ [svchost.exe]
+  TCP    [::]:5357              mylocyrus:0            LISTENING
+ Can not obtain ownership information
+  TCP    [::]:49152             mylocyrus:0            LISTENING
+ [wininit.exe]
+  TCP    [::]:49153             mylocyrus:0            LISTENING
+  eventlog
+ [svchost.exe]
+  TCP    [::]:49154             mylocyrus:0            LISTENING
+  Schedule
+ [svchost.exe]
+  TCP    [::]:49158             mylocyrus:0            LISTENING
+ [services.exe]
+  TCP    [::]:49159             mylocyrus:0            LISTENING
+ [lsass.exe]
+  TCP    [2607:fb91:68c:515d:e1ba:d3b4:6f95:ac5c]:49188  [64:ff9b::2d5a:d862]:ht
+tp  CLOSE_WAIT
+ [taskhost.exe]
+  TCP    [2607:fb91:68c:515d:e1ba:d3b4:6f95:ac5c]:49189  [64:ff9b::a2ff:7763]:ht
+tp  CLOSE_WAIT
+ [taskhost.exe]
+  TCP    [2607:fb91:68c:515d:e1ba:d3b4:6f95:ac5c]:49191  [2607:7700:0:35:0:1:a2f
+f:7755]:http  CLOSE_WAIT
+ [taskhost.exe]
+  TCP    [2607:fb91:68c:515d:e1ba:d3b4:6f95:ac5c]:49504  [64:ff9b::2d5a:d862]:ht
+tp  CLOSE_WAIT
+ [audiodg.exe]
+  TCP    [2607:fb91:68c:515d:e1ba:d3b4:6f95:ac5c]:49649  [64:ff9b::2d5a:d862]:ht
+tp  CLOSE_WAIT
+ [audiodg.exe]
+  TCP    [2607:fb91:68c:515d:e1ba:d3b4:6f95:ac5c]:53408  [2607:7700:0:35:0:1:68d
+0:105d]:http  SYN_SENT
+ [RunDll32.exe]
+  UDP    0.0.0.0:7              *:*
+ [tcpsvcs.exe]
+  UDP    0.0.0.0:9              *:*
+ [tcpsvcs.exe]
+  UDP    0.0.0.0:13             *:*
+ [tcpsvcs.exe]
+  UDP    0.0.0.0:17             *:*
+ [tcpsvcs.exe]
+  UDP    0.0.0.0:19             *:*
+ [tcpsvcs.exe]
+  UDP    0.0.0.0:161            *:*
+ [snmp.exe]
+  UDP    0.0.0.0:500            *:*
+  IKEEXT
+ [svchost.exe]
+  UDP    0.0.0.0:3702           *:*
+  FDResPub
+ [svchost.exe]
+  UDP    0.0.0.0:3702           *:*
+  EventSystem
+ [svchost.exe]
+  UDP    0.0.0.0:3702           *:*
+  FDResPub
+ [svchost.exe]
+  UDP    0.0.0.0:3702           *:*
+  EventSystem
+ [svchost.exe]
+  UDP    0.0.0.0:4500           *:*
+  IKEEXT
+ [svchost.exe]
+  UDP    0.0.0.0:5355           *:*
+  Dnscache
+ [svchost.exe]
+  UDP    0.0.0.0:52378          *:*
+  EventSystem
+ [svchost.exe]
+  UDP    0.0.0.0:61717          *:*
+  FDResPub
+ [svchost.exe]
+  UDP    0.0.0.0:61719          *:*
+  EventSystem
+ [svchost.exe]
+  UDP    127.0.0.1:1900         *:*
+  SSDPSRV
+ [svchost.exe]
+  UDP    127.0.0.1:54333        *:*
+  SSDPSRV
+ [svchost.exe]
+  UDP    192.168.249.49:137     *:*
+ Can not obtain ownership information
+  UDP    192.168.249.49:138     *:*
+ Can not obtain ownership information
+  UDP    192.168.249.49:1900    *:*
+  SSDPSRV
+ [svchost.exe]
+  UDP    192.168.249.49:54332   *:*
+  SSDPSRV
+ [svchost.exe]
+  UDP    [::]:7                 *:*
+ [tcpsvcs.exe]
+  UDP    [::]:9                 *:*
+ [tcpsvcs.exe]
+  UDP    [::]:13                *:*
+ [tcpsvcs.exe]
+  UDP    [::]:17                *:*
+ [tcpsvcs.exe]
+  UDP    [::]:19                *:*
+ [tcpsvcs.exe]
+  UDP    [::]:161               *:*
+ [snmp.exe]
+  UDP    [::]:500               *:*
+  IKEEXT
+ [svchost.exe]
+  UDP    [::]:3540              *:*
+  p2pimsvc
+ [svchost.exe]
+  UDP    [::]:3702              *:*
+  EventSystem
+ [svchost.exe]
+  UDP    [::]:3702              *:*
+  EventSystem
+ [svchost.exe]
+  UDP    [::]:3702              *:*
+  FDResPub
+ [svchost.exe]
+  UDP    [::]:3702              *:*
+  FDResPub
+ [svchost.exe]
+  UDP    [::]:4500              *:*
+  IKEEXT
+ [svchost.exe]
+  UDP    [::]:5355              *:*
+  Dnscache
+ [svchost.exe]
+  UDP    [::]:52379             *:*
+  EventSystem
+ [svchost.exe]
+  UDP    [::]:61718             *:*
+  FDResPub
+ [svchost.exe]
+  UDP    [::]:61720             *:*
+  EventSystem
+ [svchost.exe]
+  UDP    [::1]:1900             *:*
+  SSDPSRV
+ [svchost.exe]
+  UDP    [::1]:54331            *:*
+  SSDPSRV
+ [svchost.exe]
+  UDP    [fe80::31cc:fd88:28fd:aa5e%15]:1900  *:*
+  SSDPSRV
+ [svchost.exe]
+  UDP    [fe80::31cc:fd88:28fd:aa5e%15]:54330  *:*
+  SSDPSRV
+ [svchost.exe]
+
+C:\Users\mylo>
